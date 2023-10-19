df2abde1add4f8ad1dcf5b237c7bc88839f3591b
Maksim <bobreczovm@inbox.ru>
2023-10-19 21:30:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp53/BinaryHeap.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp53
+{
+    public class BinaryHeap
+    {
+        int[] heap;
+        int size;
+
+        public BinaryHeap()
+        {
+            heap = new int[120];
+            size = 0;
+        }
+
+        public void Insert(int item)
+        {
+            if (size == heap.Length)
+            {
+                ResizeHeap();
+            }
+
+            heap[size] = item;
+            size++;
+            ShiftUp(size - 1);
+        }
+
+        public int Delete(int i)
+        {
+            if (size == 0) {
+                throw new Exception("Heap is empty");
+            }
+
+
+            int root = heap[0];
+            heap[0] = heap[size - 1];
+            size--;
+            ShiftDown(i);
+            return root;
+        }
+    
+
+   private void ShiftUp(int index)
+{
+    int parentIndex = (index - 1) / 2;
+    while (parentIndex >= 0 && heap[parentIndex] < heap[index])
+    {
+        Swap(parentIndex, index);
+        index = parentIndex;
+        parentIndex = (index - 1) / 2;
+    }
+}
+
+private void ShiftDown(int index)
+{
+    while (true)
+    {
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+        int largest = index;
+
+        if (leftChildIndex < size && heap[leftChildIndex] > heap[largest])
+        {
+            largest = leftChildIndex;
+        }
+
+        if (rightChildIndex < size && heap[rightChildIndex] > heap[largest])
+        {
+            largest = rightChildIndex;
+        }
+
+        if (largest != index)
+        {
+            Swap(largest, index);
+            index = largest;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+private void Swap(int i, int j)
+{
+    int temp = heap[i];
+    heap[i] = heap[j];
+    heap[j] = temp;
+}
+
+private void ResizeHeap()
+{
+    int[] newHeap = new int[heap.Length * 2];
+    Array.Copy(heap, newHeap, heap.Length);
+      heap = newHeap;
+}
+
+
+public void Print()
+{
+    if (size == 0)
+    {
+        Console.WriteLine("PriorityQueue is empty.");
+        return;
+    }
+
+    PrintNode(0, "");
+}
+
+
+/// <summary>
+/// Отрисовка дерева
+/// </summary>
+/// <param name="currentNode"></param>
+/// <param name="indent"></param>
+private void PrintNode(int index, string indent)
+{
+    if (index < size)
+    {
+        Console.WriteLine(indent + heap[index]);
+
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+
+        if (leftChildIndex < size)
+        {
+                    Console.WriteLine(indent + "├── " + "Left Child:");
+            PrintNode(leftChildIndex, indent + "│   ");
+        }
+
+        if (rightChildIndex < size)
+        {
+                    Console.WriteLine(indent + "└── " + "Right Child:");
+            PrintNode(rightChildIndex, indent + "    ");
+        }
+    }
+
+}
+    }
+}
---
+++ ConsoleApp53/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp53
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            BinaryHeap binaryHeap = new BinaryHeap();
+            int[] d = new int[] { 4, 8, 10, 20, 5, 6, 13, 12, 25, 15, 14, 0, 26, 9, 3, 1, 19, 2, 24, 28, 17, 23, 27, 7, 11, 29, 21, 30, 22, 18, 16 };
+
+            foreach (int i in d)
+            {
+                binaryHeap.Insert(i);
+            }
+
+             Console.WriteLine("Root");
+             binaryHeap.Print();
+              binaryHeap.Delete(4);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine();
+            binaryHeap.Print();
+
+            Console.ReadLine();
+
+        }
+    }
+}
